6f615c550ac54ca517a0baea21acb4783dd56146
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-05-11 01:37:08 +0530

    Service layer and exception logic implemented

--- Account_Microservice/src/main/java/com/microservice/service/impl/AccountServiceImpl.java
+++ Account_Microservice/src/main/java/com/microservice/service/impl/AccountServiceImpl.java
@@ -1,11 +1,19 @@
 package com.microservice.service.impl;
 
+import com.microservice.constants.AccountsConstants;
 import com.microservice.dto.CustomerDto;
+import com.microservice.entity.Accounts;
+import com.microservice.entity.Customer;
+import com.microservice.exception.CustomerAlreadyExistsException;
+import com.microservice.mapper.CustomerMapper;
 import com.microservice.repository.AccountsRepository;
 import com.microservice.repository.CustomerRepository;
 import com.microservice.service.IAccountService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -16,6 +24,24 @@ public class AccountServiceImpl implements IAccountService {
 
     @Override
     public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto,new Customer());
+        Optional<Customer> optionalCustomer = customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+        if(optionalCustomer.isPresent()) {
+            throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber "
+                    +customerDto.getMobileNumber());
+        }
+        Customer savedCustomer = customerRepository.save(customer);
+        //accountsRepository.save();
+    }
 
+    private Accounts createNewAccount(Customer customer) {
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
+
+        newAccount.setAccountNumber(randomAccNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        return newAccount;
     }
 }
