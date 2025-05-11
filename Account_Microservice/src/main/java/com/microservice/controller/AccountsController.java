5a759992fd146130a1e705ee6126c4519e2d74eb
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-05-11 17:07:08 +0530

    delete api

--- Account_Microservice/src/main/java/com/microservice/controller/AccountsController.java
+++ Account_Microservice/src/main/java/com/microservice/controller/AccountsController.java
@@ -5,6 +5,7 @@ import com.microservice.dto.CustomerDto;
 import com.microservice.dto.ResponseDto;
 import com.microservice.service.IAccountService;
 import lombok.AllArgsConstructor;
+import org.hibernate.annotations.processing.Pattern;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -44,6 +45,20 @@ public class AccountsController {
             return ResponseEntity
                     .status(HttpStatus.EXPECTATION_FAILED)
                     .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_UPDATE));
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam String mobileNumber) {
+        boolean isDeleted = accountService.deleteAccount(mobileNumber);
+        if(isDeleted) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_DELETE));
         }
     }
 }
