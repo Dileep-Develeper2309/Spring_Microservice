ce02c30db3f85356ac1ffb8463014e2b1d9daf49
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-05-11 17:44:46 +0530

    auditing

--- Account_Microservice/src/main/java/com/microservice/entity/BaseEntity.java
+++ Account_Microservice/src/main/java/com/microservice/entity/BaseEntity.java
@@ -1,25 +1,37 @@
 package com.microservice.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.LocalDate;
 
 @MappedSuperclass
 @Getter @Setter @ToString
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
+    @CreatedDate
     @Column(updatable = false) //tells the spring to not update this column when we update this table
     private LocalDate createdAt;
 
+    @CreatedBy
     @Column(updatable = false)
     private String createdBy;
 
+    @LastModifiedDate
     @Column(insertable = false) //tells the spring to not update this column when the data is inserted first time
     private LocalDate updatedAt;
 
+    @LastModifiedBy
     @Column(insertable = false)
     private String updatedBy;
 }
